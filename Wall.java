7b1549dee86f2a8623d172dc945b714f73833161
Oleg Zimnik <79708122+Zimolev@users.noreply.github.com>
2021-11-11 10:54:24 +0400

    Update Wall.java
    Изменил класс Wall  с учетом вновь созданного интерфейса барьер

--- Wall.java
+++ Wall.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class Wall {
+public class Wall implements Barier {
 
     private int heigth;
 
@@ -10,5 +10,22 @@ public class Wall {
 
     public int getHeigth() {
         return heigth;
+    }
+
+    @Override
+    public boolean moving(Entyty entyty) {
+        entyty.jump();
+
+        System.out.print("Барьер высотой: " + heigth + " " +  entyty.names());
+
+        if (getHeigth() <= entyty.JumpingHeight()) {
+            System.out.println(" смог перепрыгнуть");
+
+            return true;
+        } else {
+            System.out.println(" не смог перепрыгнуть");
+
+            return false;
+        }
     }
 }
